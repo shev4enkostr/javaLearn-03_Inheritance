477f6b3bda3e2d7623ec9eeada8cf5e0b491c66f
shev4enkostr <shev4enkostr@yahoo.com>
2015-03-22 17:56:53 +0200

    add Exeption pachege  add ExeptionTest Class

---
+++ src/com/shev4enkostr/exception/ExceptionTest.java
@@ -0,0 +1,56 @@
+package com.shev4enkostr.exception;
+
+import java.util.Date;
+import java.util.*;
+
+public class ExceptionTest
+{
+	public static void main(String... args) throws ClassNotFoundException, IllegalAccessException, InstantiationException
+	{
+		try
+		{
+			Date d = new Date();
+			String className = d.getClass().getName();
+			Class cl = Class.forName(className);
+			System.out.println("Class cl = " + cl);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			System.out.println();
+		
+			Scanner in = new Scanner(System.in);
+			System.out.println("Enter class name: ");
+			String name = in.next();
+			System.out.println();
+			in.close();
+		
+			Class cl2 = Class.forName(name);
+			System.out.println("Class cl2 = " + cl2);
+		
+			Object obj = cl2.newInstance();
+			System.out.println("Object obj = " + obj);
+		}
+		
+		catch (Exception ClassNotFoundException)
+		{
+			System.out.println("Возникло исключение!");
+			System.out.println(ClassNotFoundException.getCause());
+		}
+		
+		/*Date d = new Date();
+		String className = d.getClass().toString();
+		
+		//Class cl = Class.forName(className);
+		//System.out.println("Class cl = " + cl.toString());
+		
+		//Object obj = cl.newInstance();
+		
+		Object m = Class.forName(className).newInstance();
+		*/
+	}
+}
